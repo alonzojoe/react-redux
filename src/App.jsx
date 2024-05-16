cb2df4ed557d6c1884509db65accb46404f3d106
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-05-16 16:38:41 +0800

    implement centralized store w/ reducer in counter change.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import Counter from "./components/Counter";
+import { useSelector } from "react-redux";
+const ComponentA = () => {
+  const counter = useSelector((state) => state.counter);
+  return <div style={{ color: "#fff" }}>{counter}</div>;
+};
 
 function App() {
   return (
     <>
       <Counter />
+      <ComponentA />
     </>
   );
 }
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,12 +1,32 @@
-import classes from './Counter.module.css';
-
+import classes from "./Counter.module.css";
+import { useSelector, useDispatch } from "react-redux";
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  const incrementHandler = () => {
+    dispatch({ type: "INCREMENT" });
+  };
 
+  const decrementHandler = () => {
+    dispatch({ type: "DECREMENT" });
+  };
+
+  const toggleCounterHandler = () => {};
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          margin: "10px 0",
+          gap: "5px",
+        }}
+      >
+        <button onClick={incrementHandler}>Increment +</button>
+        <button onClick={decrementHandler}>Decremeent -</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
