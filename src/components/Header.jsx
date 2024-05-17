c53e24c9888059e549fbb568fb62d1044f966fdc
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-05-17 16:10:49 +0800

    code spliting slices into separate folder and file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import classes from "./Header.module.css";
 import { useSelector, useDispatch } from "react-redux";
-import { authActions } from "../store";
+import { authActions } from "../store/slices/authSlice";
 const Header = () => {
   const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
   const dispatch = useDispatch();
