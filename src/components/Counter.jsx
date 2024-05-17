4a6897c84df119f05f8cee005f03a21575a2f1d9
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-05-17 11:31:13 +0800

    manage multiple states in redux store

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,6 +2,7 @@ import classes from "./Counter.module.css";
 import { useSelector, useDispatch } from "react-redux";
 const Counter = () => {
   const counter = useSelector((state) => state.counter);
+  const showCounter = useSelector((state) => state.showCounter);
   const dispatch = useDispatch();
   const incrementHandler = () => {
     dispatch({ type: "INCREMENT" });
@@ -11,11 +12,18 @@ const Counter = () => {
     dispatch({ type: "DECREMENT" });
   };
 
-  const toggleCounterHandler = () => {};
+  const incrementBy5 = () => {
+    dispatch({ type: "INCREASEDBY5", payload: { value: 5 } });
+  };
+
+  const toggleCounterHandler = () => {
+    const actionType = !showCounter ? "SHOWCOUNTER" : "HIDECOUNTER";
+    dispatch({ type: actionType });
+  };
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {showCounter && <div className={classes.value}>{counter}</div>}
       <div
         style={{
           display: "flex",
@@ -26,6 +34,7 @@ const Counter = () => {
       >
         <button onClick={incrementHandler}>Increment +</button>
         <button onClick={decrementHandler}>Decremeent -</button>
+        <button onClick={incrementBy5}>Inc by 5</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,19 @@
 import { act } from "react";
 import { createStore } from "redux";
 
-const initialState = { counter: 0 };
+const initialState = { counter: 0, showCounter: false };
 const counterReducer = (state = initialState, action) => {
   switch (action.type) {
     case "INCREMENT":
-      return { counter: state.counter + 1 };
+      return { ...state, counter: state.counter + 1 };
     case "DECREMENT":
-      return { counter: state.counter - 1 };
+      return { ...state, counter: state.counter - 1 };
+    case "INCREASEDBY5":
+      return { ...state, counter: state.counter + action.payload.value };
+    case "HIDECOUNTER":
+      return { ...state, showCounter: false };
+    case "SHOWCOUNTER":
+      return { ...state, showCounter: true };
     default:
       return state;
   }
